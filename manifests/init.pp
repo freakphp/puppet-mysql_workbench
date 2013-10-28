79111326c5397d19f2d3a208901dc8d8050054db
freakphp <me@freakphp.com>
2013-10-28 20:09:35 +0100

    Fix application name in init puppet

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #   include mysql_workbench
 class mysql_workbench {
-  package { 'mysql_workbench':
+  package { 'MySQLWorkbench':
     provider => 'appdmg',
     source   => 'http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.0.7.1-osx-i686.dmg/from/http://cdn.mysql.com/'
   }
