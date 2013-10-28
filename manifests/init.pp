4e576246064a71bfae8d262784b074347daaff0d
freakphp <me@freakphp.com>
2013-10-28 19:58:34 +0100

    Bump version to 6.0.7.1

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class mysql_workbench {
   package { 'mysql_workbench':
     provider => 'appdmg',
-    source   => 'http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-gpl-6.0.2-osx-i686.dmg/from/http://cdn.mysql.com/'
+    source   => 'http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.0.7.1-osx-i686.dmg/from/http://cdn.mysql.com/'
   }
 }
